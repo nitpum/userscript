493b12370f7c441e522e8790e2e7487bd5a274d8
nitpum <nitpum@outlook.com>
2024-05-11 21:45:11 +0700

    chore: Bump version

--- youtube-focus/youtube-focus.js
+++ youtube-focus/youtube-focus.js
@@ -3,7 +3,7 @@
 // @namespace   nitpum.com
 // @match       https://www.youtube.com/*
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.0.1
 // @author      nitpum
 // @description 02/03/2024, 23:35:57
 // @downloadURL https://raw.githubusercontent.com/nitpum/userscript/main/youtube-focus/youtube-focus.js
