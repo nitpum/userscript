0d9ce4345857560e6cef92172103355fc5affdeb
nitpum <nitpum@email.com>
2024-05-27 01:17:51 +0700

    feat: add bilibili remove pause overlay icon

---
+++ bilibili-remove-pause-overlay-icon/index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Bilibili remove pause overlay icon
+// @namespace   nitpum.com
+// @match       *://*.bilibili.tv/*play/*
+// @grant       GM_addStyle
+// @version     1.2.0
+// @author      nitpum
+// @description 5/26/2024, 6:52:04 PM
+// @downloadURL https://raw.githubusercontent.com/nitpum/userscript/main/bilibili-remove-pause-overlay-icon/index.js
+// ==/UserScript==
+
+GM_addStyle(`
+  .player-mobile-play-mask-pause-icon {
+    display: none !important;
+  }
+`)
