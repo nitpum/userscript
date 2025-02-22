8a14a3610696eef6095bce4714c0d1918c574353
nitpum <nitpum@outlook.com>
2025-02-22 18:56:06 +0700

    feat: youtube-focus padding top for home page

--- youtube-focus/index.js
+++ youtube-focus/index.js
@@ -14,3 +14,5 @@ GM_addStyle("ytd-reel-shelf-renderer { display: none; }");
 GM_addStyle("ytd-watch-next-secondary-results-renderer { margin-top: 80vh; }");
 
 GM_addStyle("*[is-shorts] { display: none; }");
+
+GM_addStyle("ytd-two-column-browse-results-renderer { padding-top: 100vh; }");
