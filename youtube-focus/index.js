8f120bd546739aaa13c13030ab8ab495010b3ed8
nitpum <nitpum@outlook.com>
2025-12-05 18:10:42 +0700

    chore: release youtube-focus v1.2.2

--- youtube-focus/index.js
+++ youtube-focus/index.js
@@ -3,7 +3,7 @@
 // @namespace   nitpum.com
 // @match       https://www.youtube.com/*
 // @grant       GM_addStyle
-// @version     1.2.1
+// @version     1.2.2
 // @author      nitpum
 // @description 02/03/2024, 23:35:57
 // @downloadURL https://raw.githubusercontent.com/nitpum/userscript/main/youtube-focus/index.js
