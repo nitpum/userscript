84700693d702cbcf38ceac960eae9beb5d2bf7aa
nitpum <nitpum@outlook.com>
2025-12-06 14:46:26 +0700

    fix: bilibili-hide-subtitle change download url

--- bilibili-hide-subtitle/index.js
+++ bilibili-hide-subtitle/index.js
@@ -3,10 +3,10 @@
 // @namespace   nitpum.com
 // @match       *://*.bilibili.tv/*play/*
 // @grant       GM_addStyle
-// @version     1.2.0
+// @version     1.2.1
 // @author      nitpum
 // @description 5/26/2024, 6:52:04 PM
-// @downloadURL https://raw.githubusercontent.com/nitpum/userscript/main/bilibili-hide-subtitle/index.js
+// @downloadURL https://userscript.nitpum.com/bilibili-hide-subtitle/index.js
 // ==/UserScript==
 
 const APP_ID = "#app";
