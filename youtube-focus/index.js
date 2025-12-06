7c4a7d3b286af27e312542235237ba7f70e39dd8
nitpum <nitpum@outlook.com>
2025-12-06 17:05:55 +0700

    feat(youtube-focus): hide more short and support mobile

--- youtube-focus/index.js
+++ youtube-focus/index.js
@@ -3,9 +3,9 @@
 // @namespace   nitpum.com
 // @match       https://www.youtube.com/*
 // @grant       GM_addStyle
-// @version     1.2.3
+// @version     1.3.0
 // @author      nitpum
-// @description 02/03/2024, 23:35:57
+// @description Reduce distraction on Youtube
 // @downloadURL https://userscript.nitpum.com/youtube-focus/index.js
 // ==/UserScript==
 
@@ -19,8 +19,29 @@ ytd-reel-shelf-renderer {
   display: none;
 }
 
+/* Hide mobile Shorts on home page */
+ytm-rich-section-renderer:has(ytm-shorts-lockup-view-model.shortsLockupViewModelHost) {
+  display: none;
+}
+
+/* Hide mobile Shorts on video page */
+ytm-item-section-renderer:has(ytm-shorts-lockup-view-model.shortsLockupViewModelHost) {
+  display: none;
+}
+
+/* Hide mobile Shorts button on navigation bar */
+ytm-pivot-bar-item-renderer:has(.pivot-shorts) {
+  display: none;
+}
+
+/* Hide Shorts on search page */
 ytd-search grid-shelf-view-model {
     display: none !important;
+}
+
+/* Hide mobile big Shorts on search page */
+ytm-video-with-context-renderer:has(.big-shorts-singleton) {
+  display: none !important;
 }
 
 /* Bump home screen video recommend to botom */
@@ -28,7 +49,12 @@ ytd-two-column-browse-results-renderer[page-subtype='home'] {
   padding-top: 100vh;
 }
 
-/* Sidebar video recommend */
+/* Bump home screen video mobile */
+#app .page-container:has(ytm-browse) {
+  padding-top: 100vh;
+}
+
+/* Only show sidebar video recommend when mouse hover */
 ytd-watch-next-secondary-results-renderer {
   opacity: 0;
   transition: opacity 500ms;
@@ -39,3 +65,4 @@ ytd-watch-next-secondary-results-renderer:hover {
   opacity: 1;
 }
 `);
+
