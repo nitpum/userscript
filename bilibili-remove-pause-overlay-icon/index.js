6b0bb19085828e5aee8f245aae4c9992cc6b5709
nitpum <nitpum@outlook.com>
2025-12-06 14:47:56 +0700

    fix: bilibili-remove-pause-overlay-icon change download url

--- bilibili-remove-pause-overlay-icon/index.js
+++ bilibili-remove-pause-overlay-icon/index.js
@@ -3,10 +3,10 @@
 // @namespace   nitpum.com
 // @match       *://*.bilibili.tv/*play/*
 // @grant       GM_addStyle
-// @version     1.2.0
+// @version     1.2.1
 // @author      nitpum
 // @description 5/26/2024, 6:52:04 PM
-// @downloadURL https://raw.githubusercontent.com/nitpum/userscript/main/bilibili-remove-pause-overlay-icon/index.js
+// @downloadURL https://userscript.nitpum.com/bilibili-remove-pause-overlay-icon/index.js
 // ==/UserScript==
 
 GM_addStyle(`
